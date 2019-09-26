16aac577dcf68a7c5bfec44bb0164ce4ba200037
jisudong <jisudong@show-fitness.com>
2019-09-26 10:48:19 +0800

    init project

---
+++ leetcode/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  leetcode
+//
+//  Created by 秀健身 on 2019/9/26.
+//  Copyright © 2019 jisudong. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
