0cbc1f0e6c0acc9154e23c6adb1e96261eba35cc
jisudong <136225101@qq.com>
2019-10-21 08:41:22 +0800

    86. 分隔链表

--- leetcode/linked_list.hpp
+++ leetcode/linked_list.hpp
@@ -580,4 +580,38 @@ ListNode* deleteDuplicates2(ListNode* head) {
     return dummy->next;
 }
 
+
+/**
+ 86. 分隔链表
+ 
+ 给定一个链表和一个特定值 x，对链表进行分隔，使得所有小于 x 的节点都在大于或等于 x 的节点之前。
+
+ 你应当保留两个分区中每个节点的初始相对位置。
+ 
+ https://leetcode-cn.com/problems/partition-list/
+ */
+ListNode* partition(ListNode* head, int x) {
+    ListNode *dummy1 = new ListNode(0);
+    ListNode *dummy2 = new ListNode(0);
+    ListNode *p1 = dummy1;
+    ListNode *p2 = dummy2;
+    while (head != NULL) {
+        if (head->val < x) {
+            p1->next = head;
+            head = head->next;
+            p1 = p1->next;
+        } else {
+            p2->next = head;
+            head = head->next;
+            p2 = p2->next;
+        }
+    }
+    p2->next = NULL;
+    p1->next = dummy2->next;
+    head = dummy1->next;
+    delete dummy1;
+    delete dummy2;
+    return head;
+}
+
 #endif /* linked_list_hpp */
